9ea0b27a6a0a36ae4f9ed2fd6162129fd1e38f0a
Yukino Hayakawa <tennencoll@gmail.com>
2019-03-24 10:23:34 +0800

    ignore known pdf files

--- BibTeXPaperRename.py
+++ BibTeXPaperRename.py
@@ -1,12 +1,12 @@
 import os
 import re
-import sys
+# import sys
 import bibtexparser
 import glob
 import PyPDF2
 from pathvalidate import sanitize_filename
-from pprint import pprint
-import subprocess
+# from pprint import pprint
+# import subprocess
 
 ################################ CONFIGURATION #################################
 
@@ -17,6 +17,9 @@ def make_filename(bibentry):
 
 ##################################### CODE #####################################
 
+KNOWN_FILES = set()
+UNKNOWN_FILES = []
+
 def compact_string(src):
     # https://stackoverflow.com/questions/8270092/remove-all-whitespace-in-a-string-in-python
     return re.sub(r"\s+", "", src).lower()
@@ -26,9 +29,11 @@ def collect_bib_entries():
 
     # https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python/14001395
     for bib in glob.glob("*.bib"):
-        print("Collecting BibTeX entries from {}".format(bib))
+        print("Collecting BibTeX entries from '{}'".format(bib))
         with open(bib) as bibtex_file:
             bibdb += bibtexparser.load(bibtex_file).entries
+
+    unmatched = []
 
     # remove dobule braces
     for entry in bibdb:
@@ -37,11 +42,18 @@ def collect_bib_entries():
         if title.startswith('{') and title.endswith('}'):
             # https://stackoverflow.com/questions/3085382/python-how-can-i-strip-first-and-last-double-quotes
             entry['title'] = title[1:-1]
+        filename = make_filename(entry)
+        if os.path.exists(filename):
+            # entry already has matched pdf file
+            KNOWN_FILES.add(filename)
+        else:
+            unmatched.append(entry)
 
-    return bibdb
+    return unmatched
 
 # https://medium.com/@rqaiserr/how-to-convert-pdfs-into-searchable-key-words-with-python-85aab86c544f
 def read_pdf_first_page_compact(pdfname):
+    # pypdf can't read some pdf and will produce random characters. however pdftotext works fine.
     # return subprocess.check_output(["bash", "firstpage.sh", pdfname]).decode(sys.stdout.encoding)
 
     pdfFileObj = open(pdfname, 'rb')
@@ -66,6 +78,8 @@ def find_bib_entry(bibdb, pdfname):
         if compacttitle in compactfirstpage:
             # print('Found BibTeX entry "{}" for "{}"'.format(entry['title'], pdfname))
             if longestmatch == None or len(longestmatch['title']) < len(entry['title']):
+                if longestmatch:
+                    print("Warning: {} has multiple title matches.".format(pdfname))
                 longestmatch = entry
 
     return longestmatch
@@ -77,21 +91,31 @@ def update_pdf(bibdb, pdfname):
         print('"{}" -> "{}"'.format(pdfname, newname))
         os.rename(pdfname, newname)
     else:
-        print('No BibTex entry found for "{}"'.format(pdfname))
+        UNKNOWN_FILES.append(pdfname)
 
 def update_all_pdfs(folder):
     print("Updating pdf filenames in {}".format(folder))
     os.chdir(folder)
     bibdb = collect_bib_entries()
+    print("{} BibTeX entries not matched with any pdf files:".format(len(bibdb)))
+    for entry in bibdb:
+        print("    '{}'".format(entry['title']))
+    print("Ignoring {} known pdf files".format(len(KNOWN_FILES)))
+
     # pprint(bibdb)
     for pdf in glob.glob("*.pdf"):
         filename = os.path.basename(pdf)
-        if not re.match(r'^\d\d\d\d ', filename):
+        if not filename in KNOWN_FILES:
             # print('Updating name for {}'.format(pdf))
             try:
                 update_pdf(bibdb, pdf)
             except Exception as e:
                 print(e)
+
+    if UNKNOWN_FILES:
+        print("No BibTex entry found for the following pdf files:")
+        for unknown in UNKNOWN_FILES:
+            print("    '{}'".format(unknown))
 
 def download_all_pdfs(folder):
     pass
